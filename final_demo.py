6640bfaca1808f3a5669030469594ee450577887
Adwait Shinganwade <amshinganwade@gmail.com>
2025-04-27 19:47:55 -0600

    Minor changes to demo script

--- final_demo.py
+++ final_demo.py
@@ -1,12 +1,14 @@
 import subprocess
-
+import time
 
 def main():
     # Run ausearch periodically with the checkpoint option to process previously unseen audit events
     with open("auditd-logs/logs") as logfile:
         while True:
+            print("Polling new records...\n")
             try:
-                subprocess.run(["sudo", "ausearch", "-i", "-k", "exfil-final-demo", "--checkpoint", "exfil-demo.checkpoint"], capture_output=True, text=True, stdout=logfile)
+                subprocess.run(["sudo", "ausearch", "-i", "-k", "exfil-final-demo", "--checkpoint", "exfil-demo.checkpoint"], text=True, stdout=logfile)
+                time.sleep(5)
             except KeyboardInterrupt:
                 print("Aborting...")
                 exit()
