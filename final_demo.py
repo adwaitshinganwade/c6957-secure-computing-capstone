0fe5dd9bedecc7375b22cfa2e54508a26f3fd1cd
Adwait Shinganwade <amshinganwade@gmail.com>
2025-04-27 19:36:33 -0600

    Corrected output redirection in demo script

--- final_demo.py
+++ final_demo.py
@@ -3,12 +3,13 @@ import subprocess
 
 def main():
     # Run ausearch periodically with the checkpoint option to process previously unseen audit events
-    while True:
-        try:
-            subprocess.run(["sudo", "ausearch", "-i", "-k", "exfil-final-demo", "--checkpoint", "exfil-demo.checkpoint", ">>", "auditd-logs/logs"], capture_output=False, text=False)
-        except KeyboardInterrupt:
-            print("Aborting...")
-            exit()
+    with open("auditd-logs/logs") as logfile:
+        while True:
+            try:
+                subprocess.run(["sudo", "ausearch", "-i", "-k", "exfil-final-demo", "--checkpoint", "exfil-demo.checkpoint"], capture_output=True, text=True, stdout=logfile)
+            except KeyboardInterrupt:
+                print("Aborting...")
+                exit()
 
 if __name__ == "__main__":
     main()
