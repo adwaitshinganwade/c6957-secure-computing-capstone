3ebc70bfbb50fad701665ee7cf476da44e20a8b4
Adwait Shinganwade <amshinganwade@gmail.com>
2025-04-27 20:07:58 -0600

    Corrected file mode

--- final_demo.py
+++ final_demo.py
@@ -3,11 +3,11 @@ import time
 
 def main():
     # Run ausearch periodically with the checkpoint option to process previously unseen audit events
-    with open("auditd-logs/logs") as logfile:
+    with open("auditd-logs/logs", "a") as logfile:
         while True:
             print("Polling new records...\n")
             try:
-                subprocess.run(["sudo", "ausearch", "-i", "-k", "exfil-final-demo", "--checkpoint", "exfil-demo.checkpoint"], text=True, stdout=logfile)
+                subprocess.run(["sudo", "ausearch", "-i", "--checkpoint", "exfil-demo.checkpoint"], text=True, stdout=logfile)
                 time.sleep(5)
             except KeyboardInterrupt:
                 print("Aborting...")
