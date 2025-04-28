729893992791bfd3159e924a5de9f468a5878a0e
Adwait Shinganwade <amshinganwade@gmail.com>
2025-04-27 19:23:34 -0600

    Script to tail auditd logs

--- final_demo.py
+++ final_demo.py
@@ -5,7 +5,7 @@ def main():
     # Run ausearch periodically with the checkpoint option to process previously unseen audit events
     while True:
         try:
-            subprocess.run(["sudo", "ausearch", "-i", "--checkpoint", "exfil-demo.checkpoint", ">>", "auditd-logs/logs"], capture_output=False, text=False)
+            subprocess.run(["sudo", "ausearch", "-i", "-k", "exfil-final-demo", "--checkpoint", "exfil-demo.checkpoint", ">>", "auditd-logs/logs"], capture_output=False, text=False)
         except KeyboardInterrupt:
             print("Aborting...")
             exit()
